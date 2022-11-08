5d2aaa9025c5aecfa697ca01ea1735fdc72e41f2
bidduthosen <mdbidduthosen34@gmail.com>
2022-11-08 14:41:13 +0600

    data loaded

--- index.js
+++ index.js
@@ -16,14 +16,23 @@ app.get('/', (req, res)=>{
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.og8pjeq.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    const servicesCollection = client.db('onlineDelivery').collection('services');
+    try{
+        app.get('/services', async(req, res)=>{
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const result = await cursor.limit(3).toArray();
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch((err)=> console.log(err));
 
 
 
