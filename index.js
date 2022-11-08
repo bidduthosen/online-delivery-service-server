9618221a9c56671e03db76f1b12a2c63dde01f64
bidduthosen <mdbidduthosen34@gmail.com>
2022-11-08 14:20:00 +0600

    mongodb config add and dotenv middleware use and secreat user and password setup

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+require('dotenv').config()
 
 
 app.get('/', (req, res)=>{
     res.send('online delivery service server is running');
 });
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.og8pjeq.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+
 app.listen(port, ()=>{
     console.log(`online delivery service server is running in port ${port}`)
 })
