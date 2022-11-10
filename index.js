eddd4c8b1278b5431c28c258e87de5879f7614eb
bidduthosen <mdbidduthosen34@gmail.com>
2022-11-10 11:02:09 +0600

    services new creacte api called

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ async function run(){
             const services = await cursor.toArray();
             res.send(services)
         });
+        app.post('/services', async(req, res)=>{
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            res.send(result);
+        })
 
         app.get('/services/:id', async(req, res)=>{
             const id = req.params.id;
